e24bb1d120f7c4ef3f21f8d70cf3f741422da971
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 00:40:56 -0300

    Add exercise: basic personal info

---
+++ curso_python/ex_novos/01_basic_info.py
@@ -0,0 +1,13 @@
+first_name = "Uriel"
+last_name = "Silva"
+age = 20
+birth_year = 2000
+is_adult = age >= 18
+height_meters = 1.70
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Age:", age)
+print("Birth year:", birth_year)
+print("Is an adult?", is_adult)
+print("Height in meters:", height_meters)
