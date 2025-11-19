5900a2dea79fba42132ffc889dc05468f124fceb
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 23:21:32 -0300

    Update 05_even_or_odd.py

--- curso_python/ex_novos/05_even_or_odd.py
+++ curso_python/ex_novos/05_even_or_odd.py
@@ -1,3 +1,11 @@
+# Exercise Description:
+# Create a program that asks the user to enter an integer.
+# Your program should:
+
+# Convert the input to an integer using int().
+# Check whether the number is even or odd using the modulo operator (%).
+# If the conversion fails (the user enters something that is not an integer), show an error message using try/except.
+
 try:
     integer_number = int(input("Enter an integer number: "))
 
@@ -7,3 +15,4 @@ try:
         print("The number you entered is odd!")
 except:
     print("The value you entered is not an integer!")
+
