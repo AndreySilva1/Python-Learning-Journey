b9067e6d9bebcb63dd45e1fabd34922cfa5e9139
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 23:18:46 -0300

    Update 01_basic_info.py

--- curso_python/ex_novos/01_basic_info.py
+++ curso_python/ex_novos/01_basic_info.py
@@ -1,5 +1,6 @@
 #Exercise Description:
-#Create a program that stores personal information in variables — such as first name, last name, age, birth year, and height. Then, print all this information on the screen. The program should also calculate whether the person is an adult (age ≥ 18).
+#Create a program that stores personal information in variables — such as first name, last name, age, birth year, and height.
+#Then, print all this information on the screen. The program should also calculate whether the person is an adult (age ≥ 18).
 
 first_name = "Uriel"
 last_name = "Silva"
@@ -14,4 +15,5 @@ print("Age:", age)
 print("Birth year:", birth_year)
 print("Is an adult?", is_adult)
 print("Height in meters:", height_meters)
+
 
