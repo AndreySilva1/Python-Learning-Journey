136ce6258f129af89e7b2287ea098b247ab5fca5
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 23:08:39 -0300

    Add name analysis script

---
+++ curso_python/ex_novos/04_name_analysis.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+if name and age:
+    print(f"Your name is {name}")
+    print(f"Your name reversed is {name[::-1]}")
+
+    if " " in name:
+        print("Your name contains spaces")
+    else:
+        print("Your name does not contain spaces")
+
+    print(f"Your name has {len(name)} letters")
+    print(f"The first letter of your name is {name[0]}")
+    print(f"The last letter of your name is {name[-1]}")
+else:
+    print("Sorry, you left empty fields.")
