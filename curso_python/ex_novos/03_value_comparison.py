fde0f9e1f591a427cc3b2e98e26048dbf8597e44
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 23:18:07 -0300

    Update 03_value_comparison.py

--- curso_python/ex_novos/03_value_comparison.py
+++ curso_python/ex_novos/03_value_comparison.py
@@ -1,3 +1,6 @@
+# Exercise Description:
+# Create a program that asks the user to input two values. Then, compare them and display which one is greater. If both values are equal, print a message indicating equality. The output should use f-strings to show the variable names and their values.
+
 first_value = input("Enter a value: ")
 second_value = input("Enter another value: ")
 
@@ -7,3 +10,4 @@ elif second_value > first_value:
     print(f"{second_value=} is greater than {first_value=}")
 else:
     print(f"{first_value=} is equal to {second_value=}")
+
