82da82b2339c307147ff3ee2803930555e169844
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 22:55:59 -0300

    Add BMI calculation exercise

---
+++ curso_python/ex_novos/02_bmi_calculation.py
@@ -0,0 +1,8 @@
+name = "Luiz Otávio"
+height = 1.80
+weight = 95
+bmi = weight / (height * height)
+
+print(f"{name} is {height} meters tall,")
+print(f"weighs {weight} kilograms and their BMI is")
+print(bmi)
