e9f9bcf3a8305f8c58e368ef7fb53f144e51fb88
Andrey Silva <andreyofficer@gmail.com>
2025-11-18 23:17:38 -0300

    Update 02_bmi_calculation.py

--- curso_python/ex_novos/02_bmi_calculation.py
+++ curso_python/ex_novos/02_bmi_calculation.py
@@ -1,3 +1,10 @@
+# Exercise Description:
+# Write a program that stores a person's name, height, and weight in variables. Then calculate their BMI (Body Mass Index) using the formula:
+
+# BMI = weight / (height * height)
+
+# Finally, print the information in a formatted way using f-strings.
+
 name = "Luiz Otávio"
 height = 1.80
 weight = 95
@@ -6,3 +13,4 @@ bmi = weight / (height * height)
 print(f"{name} is {height} meters tall,")
 print(f"weighs {weight} kilograms and their BMI is")
 print(bmi)
+
